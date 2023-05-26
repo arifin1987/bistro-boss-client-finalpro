61f22ef4739bde58361e4c834cefe285ea919628
Shohidul Arifin <shohidularifin@gmail.com>
2023-05-26 13:13:53 -0700

    react rating added

---
+++ src/pages/Home/Home/Featured/Featured.jsx
@@ -0,0 +1,29 @@
+import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
+import featuredImg from '../../../../assets/home/featured.jpg'
+
+const Featured = () => {
+    return (
+        <div className="featured-item bg fixed text-white pt-10 my-20">
+            <SectionTitle
+            subHeading="Check it out"
+            heading="Featured Item"
+
+            ></SectionTitle>
+            <div className="md:flex justify-center items-center pb-20 px-36 bg-slate-500 bg-opacity-60">
+            <div>
+                <img src={featuredImg} alt="" />
+            </div>
+            <div className="md:ml-10">
+                <p>Aug 20,2029</p>
+                <p className="uppercase">Where can i get some</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi tenetur sint est iste laboriosam perferendis animi error aliquid velit quia.</p>
+                <button className="btn btn-outline border-0 border-b-4">Order Now</button>
+            
+            </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Featured;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,23 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Testimonial from "../Testimonial/Testimonial";
+
+import PopularMenu from "./PopularMenu/PopularMenu";
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Category></Category>
+            <PopularMenu></PopularMenu>
+            <Testimonial></Testimonial>
+            
+            
+            
+            </div>
+    )
+};
+
+export default Home;
