36106d1fd135b08212fbed1c07f35ff223d2b428
Shohidul Arifin <shohidularifin@gmail.com>
2023-05-27 05:20:39 -0700

    custom hooks used

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,21 @@
 import {  Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/menu-bg.png'
+import PopularMenu from '../../Home/Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
+        <div>
         <Helmet>
             <title>Bistro | Menu</title>
         </Helmet>
+        <Cover img={menuImg}title="Our Menu"></Cover>
+        <PopularMenu></PopularMenu>
+        <Cover img={menuImg}title="Our Menu"></Cover>
+        <PopularMenu></PopularMenu>
+        <Cover img={menuImg}title="Our Menu"></Cover>
+        <PopularMenu></PopularMenu>
+        </div>
+        
     );
 };
 
